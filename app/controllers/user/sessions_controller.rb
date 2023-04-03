0115a8885136b9d596993b2fa7444765f93dc71a
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-04-03 13:27:53 +0000

    [update]admin/users

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -2,6 +2,8 @@
 
 class User::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :user_state, only: [:create]
+
 
   # GET /resource/sign_in
   # def new
@@ -18,7 +20,20 @@ class User::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+
+  protected
+  # 退会しているかを判断するメソッド
+  def user_state
+    @user = User.find_by(email: params[:user][:email])     #入力されたemailからアカウントを1件取得
+
+    return if !@user  #@userが取得できなかった場合はそこで終了(returnで切る)
+    if @user.valid_password?(params[:user][:password])    #@userが取得できた場合、passwordが取得したemailのアカウントと一致しているかか確認
+      #一致したので処理記述不要
+    else
+      flash[:alert] = "メールアドレスとパスワードが一致しません。"
+      redirect_to new_user_registration_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,17 @@ class User < ApplicationRecord
   has_many :bread, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_one_attached :bread_image  #ユーザー詳細でパン写真表示のため記述
+
+  def get_bread_image(width, height)
+    unless bread_image.attached?
+      file_path = Rails.root.join('app/assets/images/bread_image.jpg')
+      bread_image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
+    end
+    bread_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+
   has_one_attached :user_image
 
   def get_user_image(width, height)
