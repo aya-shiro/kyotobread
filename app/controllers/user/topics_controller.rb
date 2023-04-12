7b38732017354c41cc4f49a9e83fa58f9cc1b3dc
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-04-12 02:04:44 +0000

    [add]user/topic

--- app/controllers/user/topics_controller.rb
+++ app/controllers/user/topics_controller.rb
@@ -1,7 +1,9 @@
 class User::TopicsController < ApplicationController
   def index
+    @topics = Topic.all.order(created_at: :desc)
   end
 
   def show
+    @topic = Topic.find(params[:id])
   end
 end
