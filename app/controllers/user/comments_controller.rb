0433647f126324858acf7d32dd36f041555063d3
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-04-12 13:01:26 +0000

    [update+]header_tab

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -1,23 +1,23 @@
 class User::CommentsController < ApplicationController
   def create
-    bread = Bread.find(params[:bread_id])
+    @bread = Bread.find(params[:bread_id])
     comment = current_user.comments.new(comment_params)
-    comment.bread_id = bread.id
-
+    # comment.bread_id = @bread.id
     comment.save
+    # @bread = Bread.find(params[:bread_id])
+    # @comment = Comment.new
     # redirect_to bread_path(bread)
   end
 
   def destroy
-    comment = Comment.find(params[:bread_id])
-    bread = comment.bread
-
-    comment.destroy
+    Comment.find(params[:id]).destroy
+    # bread = comment.bread
+    @bread = Bread.find(params[:bread_id])
     # redirect_to bread_path(bread.id)
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:comment).merge(bread_id: params[:bread_id])
   end
 end
--- app/javascript/packs/kyotobread.js
+++ app/javascript/packs/kyotobread.js
@@ -1,15 +1,21 @@
-import $ from 'jquery';
-window.jQuery = $; // グローバルにjQueryを使用できるようにする
+// import $ from 'jquery';
+// window.jQuery = $; // グローバルにjQueryを使用できるようにする
 
-$('#tab-contents .tab[id != "tab1"]').hide();
+// $('#tab-contents .tab[id != "tab1"]').hide();
+$(document).on('turbolinks:load', function() {
+  $(document).ready(function () {
+    $('#tab-contents .tab[id != "tab1"]').hide();
 
-$('#tab-menu a').on('click', function(event) {
-  $("#tab-contents .tab").hide();
-  $("#tab-menu .active").removeClass("active");
-  $(this).addClass("active");
-  $($(this).attr("href")).show();
-  event.preventDefault();
+    $('#tab-menu a').on('click', function(event) {
+      $("#tab-contents .tab").hide();
+      $("#tab-menu .active").removeClass("active");
+      $(this).addClass("active");
+      $($(this).attr("href")).show();
+      event.preventDefault();
+    });
+  });
 });
+
 
 // $(document).ready(function () {
 //   $('body').html('<h1>Hello jQuery!!</h1>');
