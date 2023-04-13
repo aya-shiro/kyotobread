f852aa8334236ef212af90ec4ecabd8e8e87f739
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-04-13 03:19:16 +0000

    [update]partial_post-form

--- app/javascript/packs/kyotobread.js
+++ app/javascript/packs/kyotobread.js
@@ -1,11 +1,10 @@
-// import $ from 'jquery';
-// window.jQuery = $; // グローバルにjQueryを使用できるようにする
+import $ from 'jquery';
+window.jQuery = $; // グローバルにjQueryを使用できるようにする
 
 // $('#tab-contents .tab[id != "tab1"]').hide();
 $(document).on('turbolinks:load', function() {
   $(document).ready(function () {
     $('#tab-contents .tab[id != "tab1"]').hide();
-
     $('#tab-menu a').on('click', function(event) {
       $("#tab-contents .tab").hide();
       $("#tab-menu .active").removeClass("active");
