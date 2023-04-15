9695c24a20a72a64f2262cbe7a1e149bef95b63d
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-04-15 15:40:21 +0000

    [update]error_message

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module Kyotobread
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      html_tag
+    }
+    # エラーメッセージを表示する際、そのフィールドのHTMLをそのまま出力する
+
+
   end
 end
