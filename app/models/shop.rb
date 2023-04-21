353ac70f699a8a7b72a01c476b8648ad6179a070
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-04-21 05:45:30 +0000

    [add&update]rinku_shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -7,8 +7,8 @@ class Shop < ApplicationRecord
   # update_only: trueは、onlineモデルのカラムを”入れ替え”でなく”上書き”するために必要なオプション
 
 
-  # user/breads/newで使用
+  # user/breads/_post-formで使用
   def address_display
-    '〒' + postcode + ' ' + address + ' ' + shop_name
+    ' ' + shop_name +  '〒' + postcode + ' ' + address
   end
 end
