f154c334153bc3b03dd031a5acca57310c835b0f
aya-shiro <whitecheck00@gmail.com>
2023-05-13 06:51:20 +0000

    [update]side

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_header_data
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])    #sign_up時にnameとemailのデータを取得許可する
@@ -22,4 +23,14 @@ class ApplicationController < ActionController::Base
       root_path
     end
   end
+
+
+  private
+
+  def set_header_data
+    @shops = Shop.all.order(created_at: :desc)
+    @topics = Topic.all.order(created_at: :desc)
+    @default_drinks = Drink.default.where(default_select: true)
+  end
+
 end
