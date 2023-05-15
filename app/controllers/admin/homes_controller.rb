37f1db6a2b9ad35238184902230d9f2cbbed43d5
aya-shiro <whitecheck00@gmail.com>
2023-05-15 09:26:39 +0000

    [update]topic(&many)_responsive

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,8 +2,8 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @breads = Bread.active_users_breads.all.order(created_at: :desc)
+    @breads = Bread.active_users_breads.all.order(created_at: :desc).limit(30)
     @active_users = User.where(is_delete: false)  # 有効ユーザー数
-    @feedbacks = Feedback.all.order(created_at: :desc)
+    @feedbacks = Feedback.all.order(created_at: :desc).limit(5)
   end
 end
