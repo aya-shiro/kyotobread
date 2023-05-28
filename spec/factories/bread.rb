8ec707df7611e0e1cfa4499f60d1a95306bd25dd
aya-shiro <whitecheck00@gmail.com>
2023-05-28 13:53:19 +0000

    [update]spec02

--- spec/factories/bread.rb
+++ spec/factories/bread.rb
@@ -7,7 +7,6 @@ FactoryBot.define do
   factory :bread do
     bread_name { Faker::Lorem.characters(number:5) }
     introduce { Faker::Lorem.characters(number:20) }
-    # drink { Faker::Lorem.characters(number:20) }
     taste { Faker::Boolean.boolean }
 
     drink { association :drink, strategy: :build }
--- spec/system/02_user_after_login_spec.rb
+++ spec/system/02_user_after_login_spec.rb
@@ -14,7 +14,7 @@ describe '[STEP2] ユーザログイン後のテスト' do
   end
 
   describe 'ヘッダーのテスト: ログインしている場合' do
-    context 'リンクの内容を確認: ※logoutは『ユーザログアウトのテスト』でテスト済みになります。' do
+    context 'リンクの内容を確認: ※logoutは『ユーザログアウトのテスト』でテスト済み' do
       subject { current_path }
 
       it 'New Postを押すと、投稿画面に遷移する' do
@@ -31,5 +31,88 @@ describe '[STEP2] ユーザログイン後のテスト' do
       end
     end
   end
+
+  describe '画面のテスト' do
+    context 'マイページの確認' do
+      before do
+        visit user_path(user)
+      end
+      it '自分の名前と紹介文が表示される' do
+        expect(page).to have_content user.name
+        expect(page).to have_content user.user_info
+      end
+      it '自分のユーザ編集画面へのリンクが存在する' do
+        expect(page).to have_link '', href: users_mypage_edit_path
+      end
+      # it '自分のアカウント編集画面へのリンクが存在する' do
+      #   # expect(page).to have_link '', href: edit_user_path(user)
+      # end
+      it 'いいね一覧ボタンが表示される' do
+        expect(page).to have_link '', href: favorites_index_path
+      end
+    end
+
+    context '投稿画面のテスト' do
+      before do
+        visit new_bread_path(user)
+      end
+      it '「投稿フォーム」と表示される' do
+        expect(page).to have_content '投稿フォーム'
+      end
+      it 'bread_nameフォームが表示される' do
+        expect(page).to have_field 'bread[bread_name]'
+      end
+      it 'bread_nameフォームに値が入っていない' do
+        expect(find_field('bread[bread_name]').text).to be_blank
+      end
+      it 'introduceフォームが表示される' do
+        expect(page).to have_field 'bread[introduce]'
+      end
+      it 'introduceフォームに値が入っていない' do
+        expect(find_field('bread[introduce]').text).to be_blank
+      end
+      it '登録するボタンが表示される' do
+        expect(page).to have_button '登録する'
+      end
+    end
+    context '投稿成功のテスト' do
+      before do
+        visit breads_path(user)
+      end
+      before do
+        fill_in 'bread[bread_name]', with: bread.bread_name
+        fill_in 'bread[introduce]', with: bread.introduce
+        fill_in 'drink[drink_name]', with: bread.drink.drink_name
+        click_button '登録する'
+      end
+
+      it '自分の新しい投稿が正しく保存される' do
+        expect { click_button '登録する' }.to change(users.bread, :count).by(1)
+      end
+      it 'リダイレクト先が、保存できた投稿の詳細画面になっている' do
+        expect(current_path).to eq '/breads/' + bread.last.id.to_s
+      end
+    end
+  end
+
+  # describe 'トップ画面のテスト' do
+  #   before do
+  #     visit root_path
+  #   end
+
+    # context '表示内容の確認' do
+    #   it 'URLが正しい' do
+    #     expect(current_path).to eq '/'
+    #   end
+    #   it '自分と他人の画像のリンク先が正しい' do
+    #     expect(page).to have_link '', href: bread_path(user.bread)
+    #     expect(page).to have_link '', href: bread_path(other_user.bread)
+    #   end
+    #   it '自分の投稿と他人の投稿のタイトルのリンク先がそれぞれ正しい' do
+    #     expect(page).to have_link bread.bread_name, href: bread_path(bread)
+    #     expect(page).to have_link other_bread_name, href: bread_path(other_bread)
+    #   end
+    # end
+  # end
 
 end
