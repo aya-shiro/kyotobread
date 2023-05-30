db9f6d743fbebe5d975b5446445d9efcd8ffb0ed
aya-shiro <whitecheck00@gmail.com>
2023-05-30 14:11:55 +0000

    [update]spec03

--- spec/system/03_admin_spec.rb
+++ spec/system/03_admin_spec.rb
@@ -20,16 +20,39 @@ describe '[STEP1] アドミンログイン前のテスト' do
         expect(page).to have_link 'Log in', href: new_user_session_path
       end
     end
+  end
+  
+  describe 'ヘッダーのテスト: ログインしている場合' do
+    before do
+      visit new_user_session_path
+      fill_in 'admin[email]', with: 'fromkyoto@admin.com'
+      fill_in 'admin[password]', with: 'fromkyoto'
+      click_button 'Log in'
+    end
 
-    context 'ログイン成功のテスト' do
-      before do
-        fill_in 'admin[email]', with: 'fromkyoto@admin.com'
-        fill_in 'admin[password]', with: 'fromkyoto'
-        click_button 'Log in'
-      end
+    context 'ヘッダーの表示を確認' do
       it 'ログイン後のリダイレクト先が、ログインした状態のトップになっている' do
         expect(current_path).to eq '/admin'
       end
+      it 'KyotoBreadリンクが表示される: リンクが「KyotoBread」である' do
+        home_link = find_all('a')[0].native.inner_text
+        expect(home_link)
+      end
+      it '管理者トップリンクが表示される: リンクが「管理者トップ」である' do
+        have_link('管理者トップ', href: admin_root_path)
+      end
+      # it 'New Postリンクが表示される: リンクが「New Post」である' do
+      #   bread_link = find_all('a')[2].native.inner_text
+      #   expect(bread_link).to match(/New Post/)
+      # end
+      # it 'Usersリンクが表示される: リンクが「Users」である' do
+      #   user_users_link = find_all('a')[3].native.inner_text
+      #   expect(user_users_link).to match(/Users/)
+      # end
+      # it 'Log outリンクが表示される: リンクが「Log out」である' do
+      #   log_out_link = find_all('a')[4].native.inner_text
+      #   expect(log_out_link).to match(/Log out/)
+      # end
     end
   end
 
