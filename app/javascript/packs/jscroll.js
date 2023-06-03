38246bc4621348190945dddcd973a14e971d8302
aya-shiro <whitecheck00@gmail.com>
2023-06-03 03:34:43 +0000

    [updateJS]about

--- app/javascript/packs/jscroll.js
+++ app/javascript/packs/jscroll.js
@@ -5,57 +5,58 @@ $(document).on('turbolinks:load', function() {
   $(function() {
     // トップページの場合のみjscrollを有効にする
     if ($('.top-page').length > 0) {
+      $(window).on('scroll', function() {
+        
+        // タブ1のスクロールを処理する
+        if ($('#tab1-list').hasClass('active')) {
+          var scrollHeight = $(document).height();
+          var scrollPosition = $(window).height() + $(window).scrollTop();
+          if ((scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
+            $('.jscroll').jscroll({
+              contentSelector: '.scroll-list',
+              nextSelector: '.tab1 span.next:last a'
+            });
+          };
+        }
 
-        $(window).on('scroll', function() {
-            // タブ1のスクロールを処理する
-            if ($('#tab1-list').hasClass('active')) {
-              var scrollHeight = $(document).height();
-              var scrollPosition = $(window).height() + $(window).scrollTop();
-              if ((scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
-                $('.jscroll').jscroll({
-                  contentSelector: '.scroll-list',
-                  nextSelector: '.tab1 span.next:last a'
-                });
-              };
-            }
+        // タブ2のスクロールを処理する
+        if ($('#tab2-list').hasClass('active')) {
+          var scrollHeight2 = $(document).height();
+          var scrollPosition2 = $(window).height() + $(window).scrollTop();
+          if ((scrollHeight2 - scrollPosition2) / scrollHeight2 <= 0.05) {
+            $('.jscroll_2').jscroll({
+              contentSelector: '.scroll-list_2',
+              nextSelector: '.tab2 span.next:last a'
+            });
+          };
+        }
 
-            // タブ2のスクロールを処理する
-            if ($('#tab2-list').hasClass('active')) {
-              var scrollHeight2 = $(document).height();
-              var scrollPosition2 = $(window).height() + $(window).scrollTop();
-              if ((scrollHeight2 - scrollPosition2) / scrollHeight2 <= 0.05) {
-                $('.jscroll_2').jscroll({
-                  contentSelector: '.scroll-list_2',
-                  nextSelector: '.tab2 span.next:last a'
-                });
-              };
-            }
+        // タブ3のスクロールを処理する
+        if ($('#tab3-list').hasClass('active')) {
+          var scrollHeight3 = $(document).height();
+          var scrollPosition3 = $(window).height() + $(window).scrollTop();
+          if ((scrollHeight3 - scrollPosition3) / scrollHeight3 <= 0.05) {
+            $('.jscroll_3').jscroll({
+              contentSelector: '.scroll-list_3',
+              nextSelector: '.tab3 span.next:last a'
+            });
+          };
+        }
 
-            // タブ3のスクロールを処理する
-            if ($('#tab3-list').hasClass('active')) {
-              var scrollHeight3 = $(document).height();
-              var scrollPosition3 = $(window).height() + $(window).scrollTop();
-              if ((scrollHeight3 - scrollPosition3) / scrollHeight3 <= 0.05) {
-                $('.jscroll_3').jscroll({
-                  contentSelector: '.scroll-list_3',
-                  nextSelector: '.tab3 span.next:last a'
-                });
-              };
-            }
-
-            // タブ3のスクロールを処理する
-            if ($('#tab4-list').hasClass('active')) {
-              var scrollHeight4 = $(document).height();
-              var scrollPosition4 = $(window).height() + $(window).scrollTop();
-              if ((scrollHeight4 - scrollPosition4) / scrollHeight4 <= 0.05) {
-                $('.jscroll_4').jscroll({
-                  contentSelector: '.scroll-list_4',
-                  nextSelector: '.tab4 span.next:last a'
-                });
-              };
-            }
-        });
-
+        // タブ3のスクロールを処理する
+        if ($('#tab4-list').hasClass('active')) {
+          var scrollHeight4 = $(document).height();
+          var scrollPosition4 = $(window).height() + $(window).scrollTop();
+          if ((scrollHeight4 - scrollPosition4) / scrollHeight4 <= 0.05) {
+            $('.jscroll_4').jscroll({
+              contentSelector: '.scroll-list_4',
+              nextSelector: '.tab4 span.next:last a'
+            });
+          };
+        }
+        
+      });
     }
+    
   });
 });
